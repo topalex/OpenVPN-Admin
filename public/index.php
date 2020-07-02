e31158541195442194d2212847a6e295eb987755
Alex A. Top <topalex92@gmail.com>
2020-07-02 19:32:05 +0300

    typo

--- public/index.php
+++ public/index.php
@@ -66,7 +66,7 @@ if (isset(
         // Zip archive will be created only after closing object
         $zip->close();
 
-        //then send the headers to foce download the zip file
+        // then send the headers to force download the zip file
         header("Content-type: application/zip");
         header("Content-Disposition: attachment; filename=$archive_name");
         header("Content-Length: " . filesize($archive_path));
